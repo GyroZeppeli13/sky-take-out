3b7203790d20db7254510a785254eec8b6c8e1be
happy <984149846@qq.com>
2024-05-22 17:21:48 +0800

    新增套餐业务代码开发

--- sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -1,5 +1,6 @@
 package com.sky.mapper;
 
+import com.sky.entity.SetmealDish;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -15,4 +16,9 @@ public interface SetmealDishMapper {
      */
     List<Long> getSetmealIdsByDishIds(List<Long> dishIds);
 
+    /**
+     * 批量保持套餐与菜品关系
+     * @param setmealDishes
+     */
+    void insertBatch(List<SetmealDish> setmealDishes);
 }
